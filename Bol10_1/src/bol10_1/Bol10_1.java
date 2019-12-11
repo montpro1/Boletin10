fe897b0c9eae724971aa31fe7ee3e69fc296165d
io <io@192.168.1.36>
2019-12-11 02:01:30 +0100

    he

---
+++ Bol10_1/src/bol10_1/Bol10_1.java
@@ -0,0 +1,16 @@
+
+package bol10_1;
+
+
+public class Bol10_1 {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Xogo obx = new Xogo();
+        obx.Xogar();
+            
+        
+    }
+    
+}
