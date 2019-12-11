3711b3aaf8fbcd50bc2b9ee2d10625a9ef0754f3
io <io@192.168.1.36>
2019-12-11 02:07:52 +0100

    he21

--- Bol10_2/src/bol10_2/Xogo.java
+++ Bol10_2/src/bol10_2/Xogo.java
@@ -4,11 +4,49 @@
  * and open the template in the editor.
  */
 package bol10_2;
-
+import javax.swing.JOptionPane;
 /**
  *
  * @author io
  */
 public class Xogo {
-    
-}
+     private int num;
+    private int intentos;
+
+    public Xogo() {
+
+    }
+
+    public Xogo(int n, int intentos) {
+        num = n;
+        this.intentos = intentos;
+
+    }
+
+    public void Xogar() {
+        num = (int)(Math.random() * (50-1)+1);
+        intentos = Integer.parseInt(JOptionPane.showInputDialog("Teclee nº intentos: "));
+        int resposta;
+        do {
+            resposta = Integer.parseInt(JOptionPane.showInputDialog("Teclee resposta: "));
+            int diferenza = Math.abs(resposta - num);
+            if (diferenza > 20) {
+                JOptionPane.showMessageDialog(null, "Moi lonxe", "Pista", JOptionPane.WARNING_MESSAGE);
+            } else if (diferenza > 10) {
+                JOptionPane.showMessageDialog(null, "Lonxe", "Pista", JOptionPane.WARNING_MESSAGE);
+            } else if (diferenza > 5) {
+                JOptionPane.showMessageDialog(null, "Preto", "Pista", JOptionPane.WARNING_MESSAGE);
+            } else if (diferenza > 0) {
+                JOptionPane.showMessageDialog(null, "Moi preto", "Pista", JOptionPane.WARNING_MESSAGE);
+            } else {
+                JOptionPane.showMessageDialog(null, "Ganaches!", "PARABÉNS", JOptionPane.INFORMATION_MESSAGE);
+            }
+            intentos--;
+
+        } while (intentos > 0 && resposta != num);
+        JOptionPane.showMessageDialog(null, "Final do xogo");
+
+    }
+
+}   
+
